7f94eb6cbcda8b9e037e7b05ecd8ba44cbe8ed93
xujr3 <1781484543@qq.com>
2022-01-20 16:18:44 +0800

    毕业后第一次提交---one  submit

--- 2021-BYSJ/src/main/java/com/tjetc/domain/Admin.java
+++ 2021-BYSJ/src/main/java/com/tjetc/domain/Admin.java
@@ -1,13 +1,20 @@
 package com.tjetc.domain;
+
+import com.sun.jersey.spi.resource.Singleton;
+
 /*
-* 后台管理员表
-* */
-public class Admin extends People{
+ * 后台管理员表
+ * */
+@Singleton //此注解表示此类为，，单例类（单例模式）
+public class Admin extends People {
+
 
     private String permission;//Permission setting 权限设置
     // LOCATION:超级管理员访问   IDENTITY:普通管理员访问
 
+
     public Admin() {
+        System.out.println(" 对象被创建了.............");
     }
 
     @Override
@@ -24,4 +31,5 @@ public class Admin extends People{
     public void setPermission(String permission) {
         this.permission = permission;
     }
+
 }
