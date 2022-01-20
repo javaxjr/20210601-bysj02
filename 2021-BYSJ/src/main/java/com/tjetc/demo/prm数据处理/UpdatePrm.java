ddbdc86a258c8eec0ad2d8136b09381ebf1f83ff
xujr3 <1781484543@qq.com>
2022-01-20 16:40:51 +0800

    删除注释信息

--- 2021-BYSJ/src/main/java/com/tjetc/demo/prm数据处理/UpdatePrm.java
+++ 2021-BYSJ/src/main/java/com/tjetc/demo/prm数据处理/UpdatePrm.java
@@ -2,7 +2,9 @@ package com.tjetc.demo.prm数据处理;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
-import java.io.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,16 +24,16 @@ public class UpdatePrm {
 
     public static List<String> getFileLocalName() {
         //获取文件夹中的  所有符合条件文件
-        List<String> list  = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         File file = new File("C:\\Users\\ASUS\\Desktop\\prm\\新建文件夹");
         File[] files = file.listFiles(new FRFilenameFilerUtill());
         for (File f : files) {
-            if (f.isDirectory()){
-            }else {
+            if (f.isDirectory()) {
+            } else {
                 list.add(f.getName());
             }
         }
-        int i = list==null?0:list.size();
+        int i = list == null ? 0 : list.size();
 
         return list;
     }
@@ -42,62 +44,22 @@ public class UpdatePrm {
 
         List<String> stringList = new ArrayList<>();
 
-        /*InputStreamReader isr = null;
-
-        try {
-
-            isr = new InputStreamReader(new FileInputStream("C:\\Users\\ASUS\\Desktop\\prm\\新建文件夹\\ThirdBill_IPTV_202109020000_0001.rreq"),"GBK");
-
-            BufferedReader br = new BufferedReader(isr);
-
-            //FileReader fileReader = new FileReader(path+fileNameLists.get(0));
-
-            char[] buf = new char[2888000];
-
-            while ((br.read(buf))!=-1){
-
-                String s = new String(buf);
-                String[] split = s.split("\n");
-
-                System.out.println(s);
-                System.out.println("split.length = " + split.length);
-
-                for (int i = 0;i<split.length;i++) {
-
-                        if (split[i].trim().length()==0){
-                            continue;
-                        }
-                        stringList.add(split[i]);
-                }
-            }
-
-            //fileReader.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
-
         for (String fileNameList : fileNameLists) {
             System.out.println(fileNameList);
         }
 
-            try {
-                for (String pathname : fileNameLists) {
-                    LineIterator iterator = FileUtils.lineIterator(new File("C:\\Users\\ASUS\\Desktop\\prm\\新建文件夹\\"+pathname), "GBK");
-                    while (iterator.hasNext()){
-                        String line = iterator.nextLine();
-                        String s = line.replaceAll("-", "");
-                       // System.out.println(s);
-                    }
+        try {
+            for (String pathname : fileNameLists) {
+                LineIterator iterator = FileUtils.lineIterator(new File("C:\\Users\\ASUS\\Desktop\\prm\\新建文件夹\\" + pathname), "GBK");
+                while (iterator.hasNext()) {
+                    String line = iterator.nextLine();
+                    String s = line.replaceAll("-", "");
+                    // System.out.println(s);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         //同步文件中的信息之后，删除对应路径的文件
         return stringList;
